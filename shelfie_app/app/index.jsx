33d8e0b74bfea5a56d19c43b1b45ee7ba714549f
Mike Zahalan <mzahalan@amazon.com>
2025-05-13 11:29:26 -0700

    Lesson 2 - added Image, View, Text attributes to the page. - added basic styling. https://www.youtube.com/watch?v=UCbRTaX6i7g&list=PL4cUxeGkcC9hNTz3sxqGTfxAwU-DIHJd2&index=2

--- shelfie_app/app/index.jsx
+++ shelfie_app/app/index.jsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+
+import Logo from '../assets/img/logo_light.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img}/>
+      <Text style={[styles.title, {color: 'purple'}]}>The Number 1</Text>
+      <Text style={{marginTop: 10, marginBottom: 30}}>
+        Reading List App
+      </Text>
+      {/*
+      The Card View was a demonstartion from the second video in the tutorial.
+      We won't be using it going forward. Leaving it here for demo purposes.
+      <View>
+        <Text style={styles.card}>Hello, This is a Card.</Text>
+      </View>
+      */}
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    /*
+    card: {
+        backgroundColor: '#eee',
+        borderRadius: 5,
+        padding: 20,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)'
+    },
+    */
+    img: {
+      marginVertical: 20,
+    }
+})
