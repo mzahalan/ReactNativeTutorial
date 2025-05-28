f64c21dc1e4ed8b1035b83be3604d710e49c4e23
Mike Zahalan <mzahalan@amazon.com>
2025-05-28 15:01:41 -0700

    Lesson 25 - Using the FlatList
    - Created a FlatList in books.jsx to view the book list.

--- shelfie_app/app/(dashboard)/books.jsx
+++ shelfie_app/app/(dashboard)/books.jsx
@@ -1,17 +1,33 @@
-import { StyleSheet } from 'react-native'
+import { FlatList, Pressable, StyleSheet } from 'react-native'
 import { useBooks } from '../../hooks/useBooks'
+import { Colors } from '../../constants/colors'
 
 import Spacer from '../../components/Spacer'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
+import ThemedCard from '../../components/ThemedCard'
 
 const Books = () => {
-  const { books, getBooks } = useBooks()
+  const { books } = useBooks()
 
   return (
     <ThemedView safe={true} style={styles.container}>
-        {/*<Spacer />*/}
+      {/*<Spacer />*/}
       <ThemedText title={true} style={styles.heading}>Your Reading List</ThemedText>
+      <Spacer />
+      <FlatList
+        data={books}
+        keyExtractor={(item) => item.$id}
+        contentContainerStyle={styles.list}
+        renderItem={({ item }) => (
+          <Pressable>
+            <ThemedCard style={styles.card}>
+              <ThemedText style={styles.title}>{item.title}</ThemedText>
+              <ThemedText>Written by {item.author}</ThemedText>
+            </ThemedCard>
+          </Pressable>
+        )}
+      />
     </ThemedView>
   )
 }
@@ -21,12 +37,28 @@ export default Books
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        /*justifyContent: 'center',*/
         alignItems: 'stretch' 
     },
     heading: {
         fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    list: {
+        marginTop: 40,
+    },
+    card: {
+        width: "90%",
+        marginHorizontal: "5%",
+        marginVertical: 10,
+        padding: 10,
+        paddingLeft: 14,
+        borderLeftColor: Colors.primary,
+        borderLeftWidth: 4
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
     }
 })
